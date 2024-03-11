24b83556c0251f18dbb5c0fe3d66b0b91897c9e1
Jusinka <137655016+Jusinka@users.noreply.github.com>
2024-03-11 23:10:08 +0200

    qwe

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+    const widthContent = parseFloat(content);
+    const widthPadding = parseFloat(padding);
+    const widthBorder = parseFloat(border);
+    const totalWidth = widthContent + widthPadding * 2 + widthBorder * 2;
+    return totalWidth;
+  }
+  console.log(getElementWidth('50px', '8px', '4px')); 
+  console.log(getElementWidth('60px', '12px', '8.5px')); 
+  console.log(getElementWidth('200px', '0px', '0px')); 
